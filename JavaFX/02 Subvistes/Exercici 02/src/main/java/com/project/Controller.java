29e954394353b348ec54b3f18e31f3ab912a106d
Daniel Artiaga Perez <dartiagaperez.cf@iesesteveterradas.cat>
2024-10-14 18:01:18 +0200

    a

---
+++ JavaFX/02 Subvistes/Exercici 02/src/main/java/com/project/Controller.java
@@ -0,0 +1,23 @@
+package com.project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.event.ActionEvent;
+
+public class Controller{
+
+    @FXML
+    private ListView gameListView;
+    @FXML
+    private ImageView gameImageView;
+    @FXML
+    private Label gameNameLabel;
+    @FXML
+    private Label gameDescriptionLabel;
+    @FXML
+    private ChoiceBox options;
+
+
+    
+}
